5406bdfa2f74c2bbe9531aaece3ae40100838d58
Isk3lo <iskelo.kap@gmail.com>
2022-09-18 13:03:30 +0200

    responcive

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,16 @@ iskFunction.isWebp();
 import Swiper, { Nawigation, Pagination } from 'swiper';
 
 const swiper = new Swiper();
+
+//"use strict";
+function burgerMenu() {
+	const menu = document.querySelector('.header__menu');
+	const burger = document.querySelector('.header__burger');
+	const b = document.querySelector('body');
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('active');
+		menu.classList.toggle('active');
+		b.classList.toggle('lock');
+	})
+}
+burgerMenu();
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -9,12 +9,12 @@ export function isWebp() {
 		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 	}
 
-	testWebP(function (support) {
+	// testWebP(function (support) {
 
-		if (support == true) {
-			document.querySelector('body').classList.add('webp');
-		} else {
-			document.querySelector('body').classList.add('no-webp');
-		}
-	});
+	// 	if (support == true) {
+	// 		document.querySelector('body').classList.add('webp');
+	// 	} else {
+	// 		document.querySelector('body').classList.add('no-webp');
+	// 	}
+	// });
 }
